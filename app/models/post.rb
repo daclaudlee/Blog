ba3ffb66db6d6c2a75ea86d16f476e39c096ebae
daclaudlee <daclaud.lee5@gmail.com>
2022-08-25 14:40:42 +0430

    add the method five_most_recent_commentes to the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
 
-  def update_posts_count
+  def update_posts_counter
     author.update(posts_counter: author.posts.count)
+  end
+
+  def five_most_recent_comments
+    commentes.includes(:post).order(created_at: :desc).limit(5)
   end
 end
