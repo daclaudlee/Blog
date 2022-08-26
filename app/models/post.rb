2ad54a9ba4bbd95a8c6b4b7bd5ee266a3ecd02f1
daclaudlee <daclaud.lee5@gmail.com>
2022-08-26 15:55:07 +0430

    solve the linters issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,8 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: {greater_than_or_equal_to: 0}, {only_integer: true}
-  validates :likes_counter, numericality: {greater_than_or_equal_to: 0}, {only_integer: true}
-
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def five_most_recent_comments
     comments.includes(:post).order(created_at: :desc).limit(5)
