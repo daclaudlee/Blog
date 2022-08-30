d981f8b5b49710dc1dad346f8b02aacba2233d29
daclaudlee <daclaud.lee5@gmail.com>
2022-08-30 20:54:44 +0430

    write a test for success rendering of posts #index view file

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe "Posts", type: :request do
             get ('/users/1/posts')
             expect(response).to have_http_status(200)
         end
+
+        it "render the index template" do
+            get ('/users/1/posts')
+            expect(response).to render_template(:index)
+        end
     end
 end
