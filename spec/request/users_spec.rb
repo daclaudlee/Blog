3c3a12b508102ded3f546c6c37248be41929f688
daclaudlee <daclaud.lee5@gmail.com>
2022-08-30 20:44:33 +0430

    write a test for checking the success rendering view file after request for #show action

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -23,5 +23,10 @@ RSpec.describe 'Users', type: :request do
             get ('/users/1')
             expect(response).to have_http_status(200)
         end
+
+        it 'renders the show template' do
+            get ('/users/1')
+            expect(response).to render_template('show')
+        end
     end
 end
