42afcc44ddbea7e5ddaacd9dbdfa00f7b284949d
daclaudlee <daclaud.lee5@gmail.com>
2022-08-30 21:04:11 +0430

    solve the linters issues

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET #index' do
-        it 'returns a success response' do
-            get users_path
-            expect(response).to have_http_status(200)
-        end
-
-        it 'renders the index template' do
-            get users_path
-            expect(response).to render_template('index')
-        end
-
-        it 'renders the list of users' do
-            get users_path
-            expect(response.body).to include('Users List')
-        end
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get users_path
+      expect(response).to have_http_status(200)
     end
 
-    describe 'GET #show' do
-        it 'returns a success response' do
-            get ('/users/1')
-            expect(response).to have_http_status(200)
-        end
-
-        it 'renders the show template' do
-            get ('/users/1')
-            expect(response).to render_template('show')
-        end
-
-        it 'renders the user' do
-            get ('/users/1')
-            expect(response.body).to include('User Profile')
-        end
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template('index')
     end
+
+    it 'renders the list of users' do
+      get users_path
+      expect(response.body).to include('Users List')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a success response' do
+      get('/users/1')
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get('/users/1')
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the user' do
+      get('/users/1')
+      expect(response.body).to include('User Profile')
+    end
+  end
 end
