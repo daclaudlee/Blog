3b266dcc4f8bf1a1dbe1f029f08742f26344e3af
daclaudlee <daclaud.lee5@gmail.com>
2022-08-30 21:00:39 +0430

    write a test for success rendering of #show file for a specified post after request

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -23,5 +23,11 @@ RSpec.describe "Posts", type: :request do
             get ('/users/1/posts/1')
             expect(response).to have_http_status(200)
         end
+
+        it "render the show template" do
+            get ('/users/1/posts/1')
+            expect(response).to render_template(:show)
+        end
+
     end
 end
