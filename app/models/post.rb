f2abe7f952af98774b03caa2c05b64fbcc646f56
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 00:57:39 +0430

    add the allow_nill true for user and post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true, allow_nil: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true, allow_nil: true }
 
   def five_most_recent_comments
     comments.includes(:post).order(created_at: :desc).limit(5)
