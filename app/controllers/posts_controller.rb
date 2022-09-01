0db4cd7ae8f8edab5bd88098a85c281225429276
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 19:43:00 +0430

    add the create action controller to create a new post and save it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,4 +14,14 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def create
+    @post = Post.new(post_params)
+    @user = User.find(params[:user_id])
+    @post.user = @user
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new
+    end
+  end
 end
