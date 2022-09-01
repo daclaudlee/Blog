fbd2a06f95c46116b23070af0d19ead13a535a7c
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 19:54:08 +0430

    add the new action controller for creating new comment object

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+        @post = Post.find(params[:post_id])
+        @user = User.find(params[:user_id])
+    end
 end
