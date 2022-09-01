626ff57191fd0b59e51dcadb1f94fb5f624efefd
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 20:48:21 +0430

    solve the linters issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
-    def new
-        @comment = Comment.new
-    end
+  def new
+    @comment = Comment.new
+  end
 
-    def create
-        @comment = Comment.new(post_id: params[:post_id], author_id: current_user.id, text: params[:text])
-        @comment.save
-        if @comment.save
-            redirect_to '#'
-        end
-    end
+  def create
+    @comment = Comment.new(post_id: params[:post_id], author_id: current_user.id, text: params[:text])
+    @comment.save
+    redirect_to '#' if @comment.save
+  end
 end
