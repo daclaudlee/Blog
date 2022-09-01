6ae949b22d95bd3eb9b4c3b6132b9f4336ad3920
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 20:44:29 +0430

    add a route for adding a postv like

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/posts', to: 'posts#create'
   get '/comments/new', to: 'comments#new', as: 'new_comment'
   post '/comments/new', to: 'comments#create'
+  post '/likes/new', to: 'likes#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
