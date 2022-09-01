6effda1e0307b27cbdfac853d4136184ca5ec0ac
daclaudlee <daclaud.lee5@gmail.com>
2022-09-01 19:50:43 +0430

    add routes for new_post page and for creating post form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
