a1f5e19c7618a83f535eb717c19ad9071f657ec8
daclaudlee <daclaud.lee5@gmail.com>
2022-09-06 15:20:11 +0430

    write some tests for posts view page

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,7 +6,46 @@ RSpec.feature "Posts", type: :feature do
         @post1 = Post.create(title: 'Rspec test 1', text: 'rspec test for post', author_id: @first_user.id)
         @post2 = Post.create(title: 'Rspec test 2', text: 'rspec test for post', author_id: @first_user.id)
         @post3 = Post.create(title: 'Rspec test 3', text: 'rspec test for post', author_id: @first_user.id)
-        @post4 = Post.create(title: 'Rspec test 4', text: 'rspec test for post', author_id: @first_user.id)
     end
     
+    describe 'Post index page test' do
+        scenario 'Should show the title ' do
+        visit "/users/#{@first_user.id}/posts"
+        expect(page).to have_content(@post1.title)
+        end
+    
+        scenario 'Should show the text' do
+        visit "/users/#{@first_user.id}/posts"
+        expect(page).to have_content(@post1.text)
+        end
+    
+        scenario 'Should show the author' do
+        visit "/users/#{@first_user.id}/posts"
+        expect(page).to have_content(@first_user.name)
+        end
+    
+        scenario 'should redirect to post\'s page' do
+        visit "/users/#{@first_user.id}/posts"
+        click_on @post1.title
+        expect(page).to have_current_path("users/#{@first_user.id}/posts/#{@post1.id}")
+        end
+    end
+
+    describe 'Post show page test' do
+        scenario 'Should show the title ' do
+        visit "users/#{@first_user.id}/posts/#{@post1.id}"
+        expect(page).to have_content(@post1.title)
+        end
+    
+        scenario 'Should show the text' do
+        visit "users/#{@first_user.id}/posts/#{@post1.id}"
+        expect(page).to have_content(@post1.text)
+        end
+    
+        scenario 'Should show the author' do
+        visit "users/#{@first_user.id}/posts/#{@post1.id}"
+        expect(page).to have_content(@first_user.name)
+        end
+    end
+
 end
