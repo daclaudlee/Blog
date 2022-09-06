c848e5873bb09e95c5cc196cfedcc9659c59de52
daclaudlee <daclaud.lee5@gmail.com>
2022-09-06 15:19:49 +0430

    solved the routes issues in request specs

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before(:all) do
+    @first_user = User.create(name: 'Integration test', photo: 'http://twitter.com', bio: 'test for User')
+    @post1 = Post.create(title: 'Rspec test 1', text: 'rspec test for post', author_id: @first_user.id)
+    @post2 = Post.create(title: 'Rspec test 2', text: 'rspec test for post', author_id: @first_user.id)
+    @post3 = Post.create(title: 'Rspec test 3', text: 'rspec test for post', author_id: @first_user.id)
+  end
+
   describe 'GET #index' do
     it 'returns a success response' do
       get users_path
@@ -18,20 +25,20 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  # describe 'GET #show' do
-  #   it 'returns a success response' do
-  #     get('/users/1')
-  #     expect(response).to have_http_status(200)
-  #   end
+  describe 'GET #show' do
+    it 'returns a success response' do
+      get("/users/#{@first_user.id}")
+      expect(response).to have_http_status(200)
+    end
 
-  #   it 'renders the show template' do
-  #     get('/users/1')
-  #     expect(response).to render_template('show')
-  #   end
+    it 'renders the show template' do
+      get("/users/#{@first_user.id}")
+      expect(response).to render_template('show')
+    end
 
-  #   it 'renders the user' do
-  #     get('/users/1')
-  #     expect(response.body).to include('User Profile')
-  #   end
-  # end
+    it 'renders the user' do
+      get("/users/#{@first_user.id}")
+      expect(response.body).to include('User Profile')
+    end
+  end
 end
