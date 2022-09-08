9db34a1296900ed2287a3a0d513f8c4eebe0031d
daclaudlee <daclaud.lee5@gmail.com>
2022-09-08 18:35:00 +0430

    add the verify authenticity token to the post and comment controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @user = User.includes(:posts).find(params[:user_id])
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.role == 'admin'
+    if user.admin?
       can :manage, :all
     else
       can :read, :all
